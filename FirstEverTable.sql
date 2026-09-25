610de398bc6cf77596f5f2fce4aa70191a3883a5
MD979123 <md200720@gmail.com>
2026-09-25 13:52:28 +0100

    Update SQL commands for FirstEverTable

--- FirstEverTable.sql
+++ FirstEverTable.sql
@@ -33,7 +33,7 @@ WHERE LastName = 'Leclerc'
 INSERT INTO FirstEverTable VALUES ('Fernando','Alonso',2025, 'ALpine','A110', 1.8, 'Petrol', 249),
 ('Radu','Dragusin',2026, 'Lamborghini','Urus Performante', 4.0, 'Petrol', 657),
 ('Nico','Hulkenberg',2007, 'Skoda','Octavia', 1.9, 'Diesel', 104),
-('Bukayo','Saka',2026, 'Audi','Q8 50 TDI', 3.0, 'Diesel', 280); */
+('Bukayo','Saka',2026, 'Audi','Q8 50 TDI', 3.0, 'Diesel', 280); 
 
 UPDATE FirstEverTable
 SET Brand = 'Alpine' 
@@ -44,8 +44,23 @@ SET CarModel = 'A3 Sportback 35 TDI'
 WHERE LastName = 'Messi'
 
 INSERT INTO FirstEverTable (FirstName,LastName,CarYear,Brand,CarModel,FuelType,HorsePower )
-VALUES ('Lewis','Hamilton',2025, 'Tesla','Cybertruck', 'Electric', 845)
+VALUES ('Lewis','Hamilton',2025, 'Tesla','Cybertruck', 'Electric', 845) */
+
+
+--INSERT INTO FirstEverTable VALUES ('Phil','Foden',2025, 'Rolls-Royce','Cullinan', 6.75, 'Petrol', 563);
+
+/*INSERT INTO FirstEverTable VALUES ('George','Russell',2025, 'Mercedes','GT 63S AMG', 4.0, 'Petrol', 630),
+('Jude','Bellingham',2025, 'Aston Martin','DB12 Volante', 4.0, 'Petrol', 671),
+('Josko','Gvardiol',1999, 'Mitsubishi','Lancer Evolution VI Tommi Mäkinen Edition', 2.0, 'Petrol', 276)
+
+INSERT INTO FirstEverTable VALUES ('Steve','Johnson',2000, 'Ford','Focus', 1.8, 'Diesel', 99),
+('John','Smith',2018, 'Dacia','Sandero', 1.5, 'Diesel', 90),
+('Joe','Burton',2009, 'Citroen','C4', 2.0, 'Diesel', 138),
+('Fred','Norton',2004, 'Vauxhall','Corsa', 1.4, 'Petrol', 89); */
+
 
 SELECT *
 FROM FirstEverTable
+
+
 
