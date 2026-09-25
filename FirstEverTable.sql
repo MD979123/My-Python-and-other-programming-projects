f9c6f6e1fe53aab426051a936530f9baf455a31f
MD979123 <md200720@gmail.com>
2026-09-25 11:26:29 +0100

    Uncomment SQL commands for FirstEverTable

--- FirstEverTable.sql
+++ FirstEverTable.sql
@@ -18,7 +18,18 @@ INSERT INTO FirstEverTable VALUES ('Lionel','Messi',2017, 'Audi','A3 Sportback',
 
 UPDATE FirstEverTable
 SET CarModel = '7 Series'
-WHERE FirstName = 'Gica' */
+WHERE FirstName = 'Gica' 
 
-SELECT * 
+INSERT INTO FirstEverTable VALUES ('Sergio','Ramos',2024, 'Ferrari','296 GTB', 3.0, 'Petrol', 819),
+('Catalin','Cirjan',2022, 'BMW','5 Series 525d', 2.0, 'Diesel', 228),
+('Lewis','Hamilton',2011, 'Aston Martin','One-77', 7.3, 'Petrol', 750),
+('Max','Verstappen',2022, 'Bugatti','Chiron Pur Sport', 8.0, 'Petrol', 1500),
+('Charles','Leclerc',1995, 'Ferrari','296 GTB', 2.9, 'Petrol', 471); 
+
+UPDATE FirstEverTable
+SET CarModel = 'F40'
+WHERE LastName = 'Leclerc' */
+
+SELECT *
 FROM FirstEverTable
+ORDER BY CarYear
