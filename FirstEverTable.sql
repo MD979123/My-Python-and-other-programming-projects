a2b44eee05db8495358949184940a7376fe2dd75
MD979123 <md200720@gmail.com>
2026-09-25 11:10:37 +0100

    Update FirstEverTable with new records and corrections

--- FirstEverTable.sql
+++ FirstEverTable.sql
@@ -10,8 +10,15 @@
   HorsePower INT
 ) 
 
---INSERT INTO FirstEverTable VALUES ('Cristiano','Ronaldo',2010, 'Volkswagen','Golf', 1.6, 'Diesel', 105); */
+--INSERT INTO FirstEverTable VALUES ('Cristiano','Ronaldo',2010, 'Volkswagen','Golf', 1.6, 'Diesel', 105); 
 
-SELECT *
-FROM FirstEverTable;
+INSERT INTO FirstEverTable VALUES ('Lionel','Messi',2017, 'Audi','A3 Sportback', 2.0, 'Diesel', 150),
+('Lamine','Yamal',2016, 'Mercedes','A1 A200 CDI', 2.1, 'Diesel', 134),
+('Gica','Hagi',2019, 'BMW','7 Seires', 6.6, 'Petrol', 600); 
 
+UPDATE FirstEverTable
+SET CarModel = '7 Series'
+WHERE FirstName = 'Gica' */
+
+SELECT * 
+FROM FirstEverTable
